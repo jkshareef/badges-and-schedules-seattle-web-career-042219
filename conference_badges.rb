93b4efb922195c23e7fb79cc9e412263feb6343e
Jamal Shareef <theepie@hotmail.com>
2019-04-02 05:16:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,13 +11,20 @@ def batch_badge_creator(array)
   arr
 end
 
-def assign_rooms(array)
-  arr = []
-  counter = 1
-  for name in array
-    arr.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    counter += 1
-  end
-  arr
-end
-  
+#def assign_rooms(array)
+ # arr = []
+  #counter = 1
+  #for name in array
+   # arr.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    #counter += 1
+  #end
+  #arr
+#end
+ 
+ def assign_rooms(array)
+   arr []
+   array.each_with_index {|name, index|
+    arr.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+   }
+   arr
+ end
