830d10f66a0cda3015071adb804c7e91cf901541
Jamal Shareef <theepie@hotmail.com>
2019-04-02 05:09:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  arr = []
+  for i in array
+    arr.push(badge_maker(i))
+  end
+  arr
+end
+
+def assign_rooms(array)
+  arr = []
+  counter = 1
+  for name in array
+    arr.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  arr
+end
+  
